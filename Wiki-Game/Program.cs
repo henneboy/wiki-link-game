c85eb788d609c5150d4a2269652ed0e8bb5e4f38
henneboy <henrik_peet@hotmail.com>
2021-11-27 14:03:49 +0100

    Made it get the html of a website

---
+++ Wiki-Game/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+namespace Wiki_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string link = "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
+            Console.WriteLine(HTMLReader.Code(link));
+        }
+    }
+    public class HTMLReader
+    {
+
+        public static string Code(string Url)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            return result;
+        }
+    }
+}
