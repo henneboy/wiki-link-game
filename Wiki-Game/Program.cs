54ca31af4ca2be3b93ebf6e5132ff3c28d168c2c
henneboy <henrik_peet@hotmail.com>
2021-11-27 19:59:53 +0100

    Nearly implemented the HTML parser

--- Wiki-Game/Program.cs
+++ Wiki-Game/Program.cs
@@ -8,9 +8,15 @@ namespace Wiki_Game
     {
         static void Main(string[] args)
         {
-            string link = "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
-            Console.WriteLine(Web.Code(link));
+            string start = "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
+            //Console.WriteLine(Web.Code(start));
             List<string> links;
+            links = Web.HTMLParser(Web.Code(start));
+            foreach (string link in links)
+            {
+                Console.WriteLine(link);
+            }
+
         }
     }
     public class Web
@@ -20,6 +26,7 @@ namespace Wiki_Game
         public void Jump(string srcUrl, string dstUrl)
         {
             List<string> links;
+            unvisited.Enqueue(srcUrl);
             while (unvisited.Peek() != dstUrl)
             {
                 links = HTMLParser(Code(unvisited.Dequeue()));
@@ -31,9 +38,11 @@ namespace Wiki_Game
                     }
                 }
             }
+            Console.WriteLine("Found it");
         }
         public static string Code(string Url)
         {
+            Console.WriteLine("Visiting: " + Url);
             HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
             myRequest.Method = "GET";
             WebResponse myResponse = myRequest.GetResponse();
@@ -41,12 +50,44 @@ namespace Wiki_Game
             string result = sr.ReadToEnd();
             sr.Close();
             myResponse.Close();
-
             return result;
         }
         public static List<string> HTMLParser(string rawHTML)
         {
             List<string> links = new();
+            const string hrefStr = @"href=""/wiki";
+            const string trashStart = hrefStr + @"/Special:SpecialPages";
+            const string trashEnd = hrefStr + @"bc-editpage";
+            // remove header
+            rawHTML = rawHTML.Substring(rawHTML.IndexOf("</head>")+7);
+            rawHTML = rawHTML.Substring(0, rawHTML.IndexOf("<footer"));
+            int trashIdx;
+            while ((trashIdx = rawHTML.IndexOf(trashStart)) != -1)
+            {
+                rawHTML=rawHTML.Remove(trashIdx, (rawHTML.IndexOf(trashEnd) - trashIdx));
+            }
+            // parse the links of the body 
+            Console.WriteLine("HrefStr: " + hrefStr);
+            int hrefIndex;
+            while (true)
+            {
+                hrefIndex = -1;
+                rawHTML = rawHTML.Substring(rawHTML.IndexOf(hrefStr) + hrefStr.Length);
+                hrefIndex = rawHTML.IndexOf(@"""");
+                if (hrefIndex != -1)
+                {
+                    if (rawHTML[8] != '#')
+                    {
+                        links.Add(rawHTML.Substring(0, hrefIndex));
+                        Console.WriteLine(rawHTML.Substring(0, hrefIndex));
+                    }
+                    rawHTML = rawHTML.Substring(hrefIndex);
+                }
+                else
+                {
+                    break;
+                }
+            }
             return links;
         }
     }
